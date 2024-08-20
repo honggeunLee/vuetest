ce889cb47d818f4736ccc15ab35a020bc727e3bd
Honggeun Lee <kwy05115@gmail.com>
2024-08-20 10:16:50 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import TopMenu from './components/TopMenu.vue';
+import Navbar from './components/Navbar.vue';
+import Featured from './Featured.vue';
+import Categories from './components/Categories.vue';
+import Offer from './components/Offer.vue';
+import Products from './components/Products.vue';
+import Subscribe from './components/Subscribe.vue';
+import Products2 from './components/Products2.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <TopMenu></TopMenu>
+  <Navbar></Navbar>
+  <Featured></Featured>
+  <Categories></Categories>
+  <Offer></Offer>
+  <Products></Products>
+  <Subscribe></Subscribe>
+  <Products2></Products2>
+  <Footer></Footer>
+</template>
+
+<style scoped>
+
+</style>
